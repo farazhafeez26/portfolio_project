a1adf4a6b478811364a7505fadaaba626f6d2da4
Faraz Hafeez <fhafeez@applause.com>
2020-09-19 12:36:49 +0200

    CV changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, h
 
 const setProgress = () => {
     let scrollFromTop = (document.documentElement.scrollTop || body.scrollTop) + html.clientHeight;
-    let width = scrollFromTop / height * 100 + '%';
+    let width = (scrollFromTop / height) * 100 + '%';
 
     console.log('scroll', html.clientHeight, body.scrollTop);
 
@@ -17,3 +17,12 @@ const setProgress = () => {
 window.addEventListener('scroll', setProgress);
 
 setProgress();
+
+
+
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
