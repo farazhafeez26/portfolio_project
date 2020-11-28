4fae7df96cdd52f3625687cd024adeaf54f49b68
farazhafeez26 <faraz.hafiz@gmail.com>
2020-11-28 23:17:30 +0100

    practice for loop

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-let prog = document.getElementById('progress');
+let prog = document.getElementById("progress");
 
 let body = document.body,
-    html = document.documentElement;
+  html = document.documentElement;
 
-let height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+let height = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
 
 const setProgress = () => {
-    let scrollFromTop = (document.documentElement.scrollTop || body.scrollTop) + html.clientHeight;
-    let width = (scrollFromTop / height) * 100 + '%';
+  let scrollFromTop =
+    (document.documentElement.scrollTop || body.scrollTop) + html.clientHeight;
+  let width = (scrollFromTop / height) * 100 + "%";
 
-    console.log('scroll', html.clientHeight, body.scrollTop);
+  console.log("scroll", html.clientHeight, body.scrollTop);
 
-    prog.style.width = width;
-}
+  prog.style.width = width;
+};
 
-window.addEventListener('scroll', setProgress);
+window.addEventListener("scroll", setProgress);
 
 setProgress();
 
-
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 // Practice your JS here
 let a = "jack";
 console.log(a.length);
 
-let testFun = () => ("hahaha");
+let testFun = () => "hahaha";
 
 console.log(testFun);
 
-
-const x = 1;
+const x = "faraz";
 const y = 2;
 
-console.log(`my number is ${x}`)
+console.log(`my name is ${x}`);
 
 let now = new Date();
 
 console.log(now.getMinutes());
 console.log(typeof x);
 
+for (i = 0; i < 5; i++) {
+  console.log(i);
+  i = i + 1;
+}
